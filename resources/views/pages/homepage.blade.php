fcaf4ad95c6d5f7acea105216bd141e2e7f29895
pasqu <pasquale.bucciero97@gmailcom>
2021-06-16 12:46:37 +0200

    homePage & dettagli appartamento;

--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -3,8 +3,21 @@
 @section("content")
 <section id="homepage-test">
   <div class="container">
-    <h1>B-TEAM RULEZ</h1>
+    <h1>
+      <a href="{{route('login')}}">
+        B-TEAM RULEZ
+      </a>
+    </h1>
   </div>
 </section>
+  <ul>
+    @foreach ($apartments as $a)
+          <li>
+            <a href="{{route('apartment',$a->apartment_id)}}">
+              {{$a->apartment_id}}
+            </a>
+          </li>
+    @endforeach
 
+  </ul>
 @endsection
