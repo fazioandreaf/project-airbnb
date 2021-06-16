4d2c785d5c1d19793af56989fe1b122658484afd
Fazioandrea.f <fazioandrea.f@gmail.com>
2021-06-16 10:16:44 +0200

    1push dashboard

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,4 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 // HOMEPAGE
 Route::get('/homepage', 'MainController@homepage') -> name('homepage');
+Route::get('/dashboard', 'HomeController@dashboard') -> name('dashboard');
