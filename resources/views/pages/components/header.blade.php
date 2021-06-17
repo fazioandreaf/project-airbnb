83c18206fafc0eeaa8437c13b140eaa2c90cc5d3
Danny <danny.brogno@gmail.com>
2021-06-17 17:53:57 +0200

    scss

--- resources/views/pages/components/header.blade.php
+++ resources/views/pages/components/header.blade.php
@@ -1,3 +1,3 @@
-<div class="container">
+<div class="">
   <h1>I AM A HEADER</h1>
 </div>
--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -1,23 +1,29 @@
 @extends("layouts.main_layout")
 
 @section("content")
-<section id="homepage-test">
-  <div class="container">
-    <h1>
-      <a href="{{route('login')}}">
-        B-TEAM RULEZ
-      </a>
-    </h1>
-  </div>
-</section>
-  <ul>
-    @foreach ($apartments as $a)
-          <li>
-            <a href="{{route('apartment',$a->apartment_id)}}">
-              {{$a->apartment_id}}
-            </a>
-          </li>
-    @endforeach
 
+<div class="homepage">
+
+  <section id="homepage-test">
+    <div class="container">
+      <h1>
+        <a href="{{route('login')}}">
+          THIS IS THE MAIN
+        </a>
+      </h1>
+    </div>
+  </section>
+
+  <ul class="apartment-samples">
+    @foreach ($apartments as $a)
+      <li>
+        <a href="{{route('apartment',$a->apartment_id)}}">
+          {{$a->apartment_id}}
+        </a>
+      </li>
+    @endforeach
   </ul>
+
 @endsection
+
+</div>
