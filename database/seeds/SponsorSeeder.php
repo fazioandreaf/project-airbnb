1aee28fa07c985cb1db1fc49224e95c5386c83e0
Gioele <gioele.armiento@gmail.com>
2021-06-18 12:58:06 +0200

    Sponsor start date and expire date timezone fixed

--- database/seeds/SponsorSeeder.php
+++ database/seeds/SponsorSeeder.php
@@ -15,23 +15,23 @@ class SponsorSeeder extends Seeder
     {
 
         factory(Sponsor::class,3)->create()->each(function ($sponsor) {
-
-            $start_date = new DateTime ("now");
-            $expire = new DateTime ("now");
+            
+            $timezone = new DateTimeZone("Europe/Rome");
+            $start_date = new DateTime("now", $timezone);
+            $expire = new DateTime("now", $timezone);
+            
 
             switch ($sponsor->id) {
                 case 1:
                     $expire->modify('+24 hours');
                     break;
                 case 2:
-                    $expire->modify('+48 hours');
+                    $expire->modify('+72 hours');
                     break;
                 case 3:
                     $expire->modify('+144 hours');
                     break;
             }
-
-            // dd($timestamp);
 
             $apartments = Apartment::inRandomOrder()->limit(2)->get();
             $sponsor->apartments()->attach($apartments, ['start_date' => $start_date, 'expire_date' => $expire]);
