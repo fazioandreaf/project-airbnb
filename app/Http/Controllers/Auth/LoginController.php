a96f43ee8a6b55945b5123aafb5c2a345d6bfaf1
Fazioandrea.f <fazioandrea.f@gmail.com>
2021-06-19 11:13:08 +0200

    Logout funziona, login no

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\Auth;
-
+use Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
@@ -43,9 +43,9 @@ public function credentials(Request $request)
     {
         $this->middleware('guest')->except('logout');
     }
-    // public function logout()
-    // {
-    //     Auth::logout();
-    //     return redirect('pages/homepage');
-    // }
+    public function logout()
+    {
+        Auth::logout();
+        return redirect('pages/homepage');
+    }
 }
