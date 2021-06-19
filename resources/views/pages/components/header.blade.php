17adf8049c79bd5ef4bfa996f42ee82e10b169de
Fazioandrea.f <fazioandrea.f@gmail.com>
2021-06-19 10:26:38 +0200

    Sistemato header

--- resources/views/pages/components/header.blade.php
+++ resources/views/pages/components/header.blade.php
@@ -1,38 +1,24 @@
 <div class="outer-container">
 
-<<<<<<< Updated upstream
-  <div>
-    <span>LOGO VA QUI</span>
-    <span>
-        <a href="{{route('login')}}">
-            Log-in
-        </a>
-        <a href="{{route('register')}}">
-            DIVENTA UN HOST
-        </a>
-    </span>
-  </div>
-=======
-  <section class="bigger-header">
->>>>>>> Stashed changes
+<section class="bigger-header">
 
     <div>
-      <span>LOGO VA QUI</span>
-      <span>DIVENTA UN HOST</span>
+        <span>LOGO VA QUI</span>
+        <span>
+            <a href="{{route('login')}}">
+                Log-in
+            </a>
+            <a href="{{route('register')}}">
+                DIVENTA UN HOST
+            </a>
+
+      </span>
     </div>
-<<<<<<< Updated upstream
 
     <div>
       HEADER 2
     </div>
 
-=======
-
-    <div>
-      HEADER 2
-    </div>
-
->>>>>>> Stashed changes
   </section>
-  
+
 </div>
