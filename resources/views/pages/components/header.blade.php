8564c6ea8247dfa926f06f79a9162408253a0963
Danny <danny.brogno@gmail.com>
2021-06-19 10:17:42 +0200

    Auto stash before merge of "Danny3" and "origin/master"

--- resources/views/pages/components/header.blade.php
+++ resources/views/pages/components/header.blade.php
@@ -1,5 +1,6 @@
 <div class="outer-container">
 
+<<<<<<< Updated upstream
   <div>
     <span>LOGO VA QUI</span>
     <span>
@@ -11,16 +12,27 @@
         </a>
     </span>
   </div>
+=======
+  <section class="bigger-header">
+>>>>>>> Stashed changes
 
     <div>
       <span>LOGO VA QUI</span>
       <span>DIVENTA UN HOST</span>
     </div>
+<<<<<<< Updated upstream
 
     <div>
       HEADER 2
     </div>
 
+=======
+
+    <div>
+      HEADER 2
+    </div>
+
+>>>>>>> Stashed changes
   </section>
   
 </div>
