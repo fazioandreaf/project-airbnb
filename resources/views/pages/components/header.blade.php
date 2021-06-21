b12fcf9807328960af8c8f2d2900c9884166a148
Danny <danny.brogno@gmail.com>
2021-06-21 19:20:56 +0200

    Auto stash before merge of "master" and "origin/master"

--- resources/views/pages/components/header.blade.php
+++ resources/views/pages/components/header.blade.php
@@ -1,20 +1,15 @@
-<div class="outer-container">
-  <div class="bigger-header">
-    <div> <!-- Questo div è header con logo/diventaHost/Login -->
-      <span>
+<div class="outer-container">  <div class="bigger-header">    <div> <!-- Questo div è header con logo/diventaHost/Login -->      <span>
         <a href="{{route('homepage')}}">
           <img src="{{asset('images/lg_color0.png')}}" alt="logo-image">
         </a>
-      </span>
-      <span>
+      </span>      <span>
         <!-- LOGIN NO BOOTSTRAP - DA IMPLEMENTARE <UL><LI> ESSENDO UN MENU -->
         @guest
           @if (Route::has('register'))
             <a href="{{ route('register') }}">
               {{ __('Diventa un Host') }}
             </a>
-          @endif
-          <a href="{{ route('login') }}">
+          @endif          <a href="{{ route('login') }}">
             <i class="fas fa-bars"></i>
             <i class="fas fa-user"></i>
           </a>
@@ -28,8 +23,7 @@
               document.getElementById('form_logout').submit();"
           >
               {{ __('Logout') }}
-          </a>
-          <form id="form_logout" method="POST" action="{{ route('logout') }}">
+          </a>          <form id="form_logout" method="POST" action="{{ route('logout') }}">
               @csrf
           </form>
           <div>
@@ -38,33 +32,26 @@
               </a>
           </div>
         @endauth
-    </div>
-    <div> <!-- INIZIO div in header con la navbar1 (width 100%)-->
-      <div class="navbar1"> <!-- INIZIO Navbar1 (width 70%)-->
-        <div>
+    </div>    <div> <!-- INIZIO div in header con la navbar1 (width 100%)-->
+      <div class="navbar1"> <!-- INIZIO Navbar1 (width 70%)-->        <div>
           <a href="#">
             <h3>Dove</h3>
             <p>Dove vuoi andare?</p>
           </a>
-        </div>
-        <div>
+        </div>        <div>
           <a href="#">
             <h3>Check-In</h3>
             <p>Aggiungi data</p>
           </a>
-        </div>
-        <div>
+        </div>        <div>
           <a href="#">
             <h3>Check-Out</h3>
             <p>Aggiungi data</p>
           </a>
-        </div>
-        <div>
-          <a href="#">
+        </div>        <div>          <a href="#">
             <h3>Ospiti</h3>
             <p>Aggiungi ospiti</p>
-          </a>
-          <a href="{{route('search')}}">
+          </a>          <a href="{{route('search')}}">
             <i class="fas fa-search"></i>
           </a>
         </div>
