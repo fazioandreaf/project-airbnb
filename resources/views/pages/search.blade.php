a19c89682f2eeee367e8a45e807ad1adc2acd6a0
Fazioandrea.f <fazioandrea.f@gmail.com>
2021-06-21 15:10:43 +0200

    aggiunto card in search

--- resources/views/pages/search.blade.php
+++ resources/views/pages/search.blade.php
@@ -5,12 +5,23 @@ style="display: flex;
 justify-content:center">
 
     <div
-    style="display: flex:1;
-    height:400px;background-color:green;color:white">
+    style="display:flex; flex-basis:50%; flex-wrap: wrap;
+    height:400px;background-color:green;color:white;overflow: scroll;">
         Inserimento di card che fanno comparire gli appartament disponibili, come boolflix
+        @foreach ($apartments as $item)
+            <div
+                style="margin:5px;background-color:lightgrey; flex-basis:20%">
+                <strong>title: </strong>{{$item->title}}
+                <strong>number_rooms: </strong>{{$item->number_rooms}}
+                <strong>number_toiletes: </strong>{{$item->number_toiletes}}
+                <strong>number_beds: </strong>{{$item->number_beds}}
+                <strong>area: </strong>{{$item->area}}
+                <strong>address: </strong>{{$item->address}}
+            </div>
+        @endforeach
     </div>
     <div
-    style="display: flex:1;
+    style="display: flex-basis:50%;
     height:400px;background-color:purple;color:white">
         show maps
     </div>
