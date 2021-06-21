3b6b829dd819cd97f61986d24f6a79edc7b09e97
Fazioandrea.f <fazioandrea.f@gmail.com>
2021-06-21 10:35:41 +0200

    registeer login e logout funzionanti

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -8,44 +8,20 @@ use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
-
     use AuthenticatesUsers;
 
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
     protected $redirectTo = RouteServiceProvider::HOME;
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-public function credentials(Request $request)
-    {
-        $credentials = $request->only($this->email(), 'firstname','lastname');
-        $credentials = array_add($credentials, 'status', '1');
-        return $credentials;
-    }
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
     }
+
     public function logout()
     {
         Auth::logout();
         return redirect('pages/homepage');
     }
 }
+
+
