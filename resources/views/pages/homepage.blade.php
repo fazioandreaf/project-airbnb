7be8fffda39497393b9b5945af7ee17390c6b5d3
Danny <danny.brogno@gmail.com>
2021-06-21 12:58:26 +0200

    prova76

--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -2,52 +2,41 @@
 
 @section("content")
 
-<div class="jumbotron">
-  <section>
+  <div class="jumbotron">
+    <section>
 
-    <ul>
+      <ul>
+        <li>
+          <a href="{{route('login')}}">
+            <h1>BoolB&B</h1>
+          </a>
+          <span>2021</span>
+        </li>
 
-      <li>
-        <a href="{{route('login')}}">
-          <h1>BoolB&B</h1>
-        </a>
-        <span>2021</span>
-      </li>
+        <li>
+          <h2>Ti presentiamo<div>oltre 100 novità</div></h2>
+        </li>
+      </ul>
 
-      <li>
-        <h2>Ti presentiamo<div>oltre 100 novità</div></h2>
-      </li>
-    </ul>
-
-
-        {{-- a href="{{route('search')}}">Search advanced</a> --}}
-      </h1>
-    </div>
-
-    {{-- rotta per la create --}}
-    <a href="{{route('add')}}">
-      <button style="width: 300px">
-        NEW APARTMENT
-      </button>
-    </a>
-
-  </section>
-
-
-</div>
-
-<div class="outer-jumbotron">
-
-  <ul class="apartment-samples">
-    @foreach ($apartments as $apartment)
-    <li>
-      <a href="{{route('apartment', $apartment->id)}}">
-        {{ $apartment->id }}
-        {{ $apartment->title }}
+      {{-- rotta per la create --}}
+      <a href="{{route('add')}}">
+        <button style="width: 300px">
+          NEW APARTMENT
+        </button>
       </a>
-    </li>
-    @endforeach
-  </ul>
+    </section>
+  </div>
 
-</div>
+  <div class="outer-jumbotron">
+    <ul class="apartment-samples">
+      @foreach ($apartments as $apartment)
+      <li>
+        <a href="{{route('apartment', $apartment->id)}}">
+          {{ $apartment->id }}
+          {{ $apartment->title }}
+        </a>
+      </li>
+      @endforeach
+    </ul>
+  </div>
 @endsection
