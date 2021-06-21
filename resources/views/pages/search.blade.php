29f23467fd9bcc7aa0953b53b2975de675f57674
Danny <danny.brogno@gmail.com>
2021-06-21 18:59:38 +0200

    eliminazione immagini

--- resources/views/pages/search.blade.php
+++ resources/views/pages/search.blade.php
@@ -1,5 +1,16 @@
-@extends('layouts.main_layout')
+@extends('layouts.search_layout')
 @section('content')
-    advanced Search
-    Inserimento form
+  <div class="search-page">
+
+    <!-- UPPER SECTION STARTS HERE -->
+    <div class="upper-section">
+      MAIN 1 test
+    </div>
+
+    <!-- LOWER SECTION STARTS HERE -->
+    <div class="lower-section">
+      MAIN 2 test
+    </div>
+
+  </div>
 @endsection
