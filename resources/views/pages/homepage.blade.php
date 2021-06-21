89be70d083c09b5f9c14dc7fd3fd8052bf09db07
pasqu <pasquale.bucciero97@gmailcom>
2021-06-21 15:19:55 +0200

    homepage

--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -4,31 +4,6 @@
 
   <div class="jumbotron">
     <section>
-<<<<<<< HEAD
-
-      <ul>
-        <li>
-          <a href="{{route('login')}}">
-            <h1>BoolB&B</h1>
-          </a>
-          <span>2021</span>
-        </li>
-
-        <li>
-          <h2>Ti presentiamo<div>oltre 100 novità</div></h2>
-        </li>
-      </ul>
-
-      {{-- rotta per la create --}}
-      <a href="{{route('add')}}">
-        <button style="width: 300px">
-          NEW APARTMENT
-        </button>
-      </a>
-    </section>
-  </div>
-
-=======
 
       <ul>
         <li>
@@ -53,7 +28,6 @@
     </section>
   </div>
 
->>>>>>> Danny7
   <div class="outer-jumbotron">
     <ul class="apartment-samples">
       @foreach ($apartments as $apartment)
@@ -65,33 +39,5 @@
       </li>
       @endforeach
     </ul>
-<<<<<<< HEAD
-
-    {{-- rotta per la create --}}
-    <a href="{{route('add')}}">
-      <button style="width: 300px">
-        NEW APARTMENT
-      </button>
-    </a>
-
-
-  </section>
-</div>
-
-<div class="outer-jumbotron">
-  <ul class="apartment-samples">
-    @foreach ($apartments as $apartment)
-    <li>
-      <a href="{{route('apartment', $apartment->id)}}">
-        {{ $apartment->id }}
-        {{ $apartment->title }}
-      </a>
-    </li>
-    @endforeach
-  </ul>
-</div>
-
-=======
   </div>
->>>>>>> Danny7
 @endsection
