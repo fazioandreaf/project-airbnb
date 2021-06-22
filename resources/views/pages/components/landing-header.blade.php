8669b76d4b4e004bf1434e8671ffb39b450a908f
Gioele <gioele.armiento@gmail.com>
2021-06-22 18:24:48 +0200

    Search bar added

--- resources/views/pages/components/landing-header.blade.php
+++ resources/views/pages/components/landing-header.blade.php
@@ -1,46 +1,96 @@
 <header class="landing-header">
     <img src="{{ asset('storage/assets/wallpaper2.webp') }}">
     <nav class="navbar">
-        <ul class="nav-links">
-            <li>
-                <a href="{{ route('homepage') }}">
-                    <img src="{{ asset('storage/assets/lg_color0.png')}}" alt="Logo BoolBnB">
-                </a>
-            </li>
-            <li>
-                {{-- Guest View --}}
-                @guest
-                    @if (Route::has('register'))
-                    <a href="{{ route('register') }}">
-                    {{ __('Diventa un Host') }}
+        <div class="container">
+            <ul class="nav-links">
+                <li id="wrapper-logo">
+                    <a href="{{ route('homepage') }}">
+                        <img src="{{ asset('storage/assets/lg_color0_bianco.png')}}" alt="Logo BoolBnB">
                     </a>
-                    @endif    
-                <a href="{{ route('login') }}">
-                    <i class="fas fa-bars"></i>
-                    <i class="fas fa-user"></i>
-                    </a>
-                @endguest
-                {{-- Logged view --}}
-                @auth
-                    <a href="#">
-                    {{ Auth::user()->name }}
-                    </a>
-                    <a href="{{ route('logout')}}" onclick="
-                        event.preventDefault();
-                        document.getElementById('form_logout').submit();"
-                    >
-                        {{ __('Logout') }}
-                    </a>
-                    <form id="form_logout" method="POST" action="{{ route('logout') }}">
-                        @csrf
-                    </form>
-            </li>
-            <li>
-                <a href="{{ route('dashboard',Auth::id()) }}">
-                    Dashboard
-                </a>
-            </li>
-                @endauth
-        </ul>
+                </li>
+                <li class="has-dropdown">
+                    <div class="hamburger">
+                        <i class="fas fa-bars icon"></i>
+                        <i class="fas fa-user icon"></i>
+                    </div>
+                    <!-- Dropdown -->
+                    <ul class="dropdown">
+                        <li>
+                            {{-- Guest View --}}
+                            @guest
+                                @if (Route::has('register'))
+                                    <a href="{{ route('register') }}">
+                                        {{ __('Diventa un Host') }}
+                                    </a>
+                                @endif  
+                        </li>
+                        <li>
+                            <a href="{{ route('login') }}">
+                                {{ __('Login') }}
+                            </a>
+                            @endguest
+                        </li>  
+                        <li>
+                            {{-- Logged view --}}
+                            @auth
+                                Ciao, <strong>{{ Auth::user()->firstname }}</strong>!
+                        </li>
+                        <li>
+                            <a href="{{ route('dashboard',Auth::id()) }}">
+                                {{ __('Dashboard') }}
+                            </a>
+                        </li>
+                        <li>
+                            <a href="{{ route('logout')}}" onclick="
+                                    event.preventDefault();
+                                    document.getElementById('form_logout').submit();"
+                                >
+                                    {{ __('Logout') }}
+                                </a>
+                                <form id="form_logout" method="POST" action="{{ route('logout') }}">
+                                    @csrf
+                                </form>
+                            @endauth
+                        </li>
+                    </ul> <!-- end dropdown -->
+                </li>
+            </ul>
+
+            <div class="wrapper-search-bar">
+                <form class="search-bar" action="{{ route('search') }}" method="post" enctype="multipart/form-data">
+                    @method('POST')
+                    @csrf
+                    <div class="wrapper-form-fields first">
+                        <label for="where">
+                            Dove
+                        </label>
+                        <input type="text" id="where" name="where" placeholder="Dove vuoi andare?">
+                    </div>
+                    <div class="wrapper-form-fields">
+                        <label for="check_in">
+                            Check-in
+                        </label>
+                        <input type="date" id="check_in" name="check_in" placeholder="Aggiungi date">
+                    </div>
+                    <div class="wrapper-form-fields">
+                        <label for="check_out">
+                            Check-out
+                        </label>
+                        <input type="date" id="check_out" name="check_out" placeholder="Aggiungi date">
+                    </div>
+                    <div class="wrapper-form-fields">
+                        <label for="guest">
+                            Ospiti
+                        </label>
+                        <input type="number" min=1 id="guest" name="guest" value="1" placeholder="Aggiungi ospiti">
+                    </div>
+                    <div class="wrapper-form-fields last">
+                        <button type="submit" class="search">
+                            <i class="fas fa-search"></i>
+                        </div>
+                    </div>   
+                </form>
+            </div>
+        </div>
     </nav>
 </header>
