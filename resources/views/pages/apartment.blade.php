38b8e6ebb079e32de3ef3ee1d28aaba72994ab8e
Danny <danny.brogno@gmail.com>
2021-06-23 17:37:37 +0200

    _APARTMENT.BLADE.PHP
    nuova pagina "_apartment.blade.php" iniziata (header e jumbotron)

--- resources/views/pages/apartment.blade.php
+++ resources/views/pages/apartment.blade.php
@@ -1,81 +1,140 @@
-@extends('layouts.main_layout')
-@section('content')
-    <div class="container">
-        <h1>
-            [{{$apartment->id}}] -- {{$apartment->title}}
-            {{-- rotta soft-delete --}}
-            <a href="{{route('delete',$apartment->id)}}">
-                <button>
-                    --delete--
-                </button>
+@extends('layouts.apartment_layout')
+@section('content_apartment')
+
+    {{-- DANNY --}}
+    <div class="container" id="apartment-sample">
+
+      {{-- _________ HEADER INIZIA QUI__________ --}}
+      <nav>
+        <div class="basic-header">
+
+          <div>
+            <span>
+              <a href="{{route('homepage')}}">
+                <img src="{{asset('storage/assets/lg_clr.png')}}" alt="logo-image">
+              </a>
+            </span>
+          </div>
+
+          <div>
+
+            <span id="barra-ricerca">
+              <a href="#">
+                Inizia la ricerca
+              </a>
+              <a href="#">
+                <i class="fas fa-search"></i>
+              </a>
+            </span>
+
+          </div>
+
+          <div>
+            @guest
+              @if (Route::has('register'))
+                <a href="{{ route('register') }}">
+                  {{ __('Diventa un Host') }}
+                </a>
+              @endif
+              <a href="{{ route('login') }}">
+                <i class="fas fa-bars"></i>
+                <i class="fas fa-user"></i>
+              </a>
+            @endguest
+            @auth
+              <a href="#">
+                {{ Auth::user()->name }}
+              </a>
+              <a href="{{ route('logout')}}" onclick="
+              event.preventDefault();
+              document.getElementById('form_logout').submit();"
+              >
+              {{ __('Logout') }}
             </a>
-            {{-- rotta per la update --}}
-            <a href="{{route('edit',$apartment->id)}}">
-                <button>
-                --edit--
-                </button>
-            </a>
-        </h1>
-                    {{-- form per l'aggiunta sponsor --}}
-        <div>
-            <form action="{{Route('sponsor_function',$apartment->id)}}" method="get">
-                <select name="sponsor_id" id="sponsor_id">
-                    @foreach ($sponsors as $sponsor)
-                        <option value="{{$sponsor->id}}">
-                            {{$sponsor->sponsor_duration}} H
-                        </option>
-                    @endforeach
-                </select>
-                <button type="submit">Get</button>
+            <form id="form_logout" method="POST" action="{{ route('logout') }}">
+              @csrf
             </form>
+            <div>
+              <a href="{{route('dashboard',Auth::id())}}">
+                Dashboard
+              </a>
+            </div>
+          @endauth
         </div>
 
-        <div class="test">
-            <img src="{{asset('storage/apartment-img/'.$apartment -> cover_image)}}" alt="">
-        </div>
+      </div> {{-- FINE BASIC HEADER--}}
+    </nav> {{-- FINE NAV--}}
+    {{-- _________ HEADER FINISCE QUI__________ --}}
 
-        <ul>
-            <li>
-                Stanza :
-                {{$apartment->number_rooms}}
-            </li>
-            <li>
-                Numero letti :
-                {{$apartment->number_beds}}
-            </li>
-            <li>
-                numero bagni :
-                {{$apartment->number_toiletes}}
-            </li>
-            <li>
-                Indirizzo :
-                [{{$apartment->address}}]
-            </li>
-            <li>
-                area :
-                {{$apartment->area}}
-            </li>
-        </ul>
+    {{-- _________JUMBOTRON INIZIA QUI__________ --}}
 
-        <ul>
-            @foreach ($apartment->services as $service)
-                <li>
-                    {{$service->service}}
-                </li>
-            @endforeach
-        </ul>
+      <div class="jumbotron-apartment">
 
-        @foreach ($apartment->sponsors as $apSp)
-            <p>
-                {{$apSp->sponsor_duration}} durata sponsor
-            </p>
-        @endforeach
-        <div>
-            <h1>FORM INVIO MESSAGGIO
-                <a href="{{route('send',$apartment->id)}}"
-                style="background-color:blue; padding:2rem"
-                    >SUBMIT INVIO MESSAGGIO</a>
+        <div id="jumbo-container">
+
+          <div class="titolo">
+            <h1>
+              {{-- <div>ID:[{{$apartment->id}}]</div> --}}
+              {{$apartment->title}}
             </h1>
+          </div>
+
+
+          {{-- <div class="test">
+            <img src="{{asset('storage/apartment-img/'.$apartment -> cover_image)}}" alt="">
+          </div>
+
+          <ul>
+            <li>
+              Stanza :
+              {{$apartment->number_rooms}}
+            </li>
+            <li>
+              Numero letti :
+              {{$apartment->number_beds}}
+            </li>
+            <li>
+              numero bagni :
+              {{$apartment->number_toiletes}}
+            </li>
+            <li>
+              Indirizzo :
+              [{{$apartment->address}}]
+            </li>
+            <li>
+              area :
+              {{$apartment->area}}
+            </li>
+          </ul>
+
+          <ul>
+            @foreach ($apartment->services as $service)
+              <li>
+                {{$service->service}}
+              </li>
+            @endforeach
+          </ul>
+
+          <h5>
+            @foreach ($apartment->sponsors as $apSp)
+              <p>
+                {{$apSp->sponsor_duration}} durata sponsor
+              </p>
+            @endforeach
+          </h5>
+
+          <div>
+            <h5>FORM INVIO MESSAGGIO
+              <a href="{{route('send',$apartment->id)}}"
+                >SUBMIT INVIO MESSAGGIO</a>
+            </h5>
+          </div> --}}
+
         </div>
+
+      </div>
+        {{-- _________JUMBOTRON FINISCE QUI__________ --}}
+
+
     </div>
-@endsection
+  @endsection
