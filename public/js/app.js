cb2231c5763bd279624f15254fbc41066cb05835
titty91 <tznbaraldi@gmail.com>
2021-06-24 10:57:14 +0200

    .

--- public/js/app.js
+++ public/js/app.js
@@ -50262,8 +50262,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Danny\Progetti_2021\06 -2021\15-06-2021\project-airbnb\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Danny\Progetti_2021\06 -2021\15-06-2021\project-airbnb\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\redsy\Documents\Boolean\progetto-finale\Nuova cartella\project-airbnb\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\redsy\Documents\Boolean\progetto-finale\Nuova cartella\project-airbnb\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
