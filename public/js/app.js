bbed878e1a51874996c143605918cb33aeea40dd
Fazioandrea.f <fazioandrea.f@gmail.com>
2021-06-25 18:00:22 +0200

    aggiunto apicontroller

--- public/js/app.js
+++ public/js/app.js
@@ -50000,6 +50000,7 @@ document.addEventListener('DOMContentLoaded', function () {
   var app = new Vue({
     el: '#app',
     data: {
+      labelActive: "profile-infos",
       dropdownActive: false,
       registerErrors: [],
       firstname: null,
@@ -50272,8 +50273,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Boolean\project-airbnb\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Boolean\project-airbnb\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\Andrea\Desktop\project-airbnb\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\Andrea\Desktop\project-airbnb\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
