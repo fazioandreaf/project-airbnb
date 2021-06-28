9dcfe899cc648e3d9c069ae8dadc2468ada371e0
Danny <danny.brogno@gmail.com>
2021-06-28 18:04:45 +0200

    progressi DY

--- resources/views/layouts/search_layout.blade.php
+++ resources/views/layouts/search_layout.blade.php
@@ -35,185 +35,23 @@
   </head>
 
   <body>
-      <div id="search">
+    <div id="search">
 
-          <header id="header-search">
-              <div class="top-header-search">
-                  <div class="logo">
-                      <a href="{{route('homepage')}}">
-                          <img src="{{asset('storage/assets/lg_clr.png')}}" alt="logo-image">
-                      </a>
-                  </div>
-                  <div class="filter">
-                          <div>
-                              <label for="where">
-                                  Scrivi l'indirizzo
-                              </label>
-                              <input type="text" v-model="where" name="where" placeholder="dove">
-                          </div>
+      <header>
+        @include('pages.components.search_header')
+      </header>
 
-                          <div class="wrapper-form-fields first">
-                              <label for="number_rooms">
-                                  Numeri di stanze
-                              </label>
-                              <input type="number" v-model="number_rooms" name="number_rooms" placeholder="1">
-                          </div>
-                          <div>
-                              <label for="number_beds">
-                                  Numeri di letti
-                              </label>
-                              <input type="number" v-model="number_beds" name="number_beds" placeholder="1">
-                          </div>
-                          <div>
-                              <a href="#" @click="filter()">
+      <main>
+        @yield('content')
+      </main>
 
-                                  <i class="fas fa-search"></i>
-                              </a>
-                          </div>
-                  </div>
-                  <div>
-                      @guest
-                      @if (Route::has('register'))
-                          <a href="{{ route('register') }}">
-                          {{ __('Diventa un Host') }}
-                          </a>
-                      @endif
-                      <a href="{{ route('login') }}">
-                          <i class="fas fa-bars"></i>
-                          <i class="fas fa-user"></i>
-                      </a>
-                      @endguest
-                      @auth
-                      <a href="#">
-                          {{ Auth::user()->name }}
-                      </a>
-                      <a href="{{ route('logout')}}" onclick="
-                          event.preventDefault();
-                          document.getElementById('form_logout').submit();"
-                      >
-                          {{ __('Logout') }}
-                      </a>
-                      <form id="form_logout" method="POST" action="{{ route('logout') }}">
-                          @csrf
-                      </form>
-                      <div>
-                          <a href="{{route('dashboard',Auth::id())}}">
-                              Dashboard
-                          </a>
-                      </div>
-                      @endauth
-                  </div>
-              </div>
-              <div class="lower-header-search">
-                  {{-- <ul
-                  style="display: flex;
-                  justify-content: center;">
-                      @foreach ($services as $service)
-                          <li>
-                              <a href="#" @click="activeService($service)">
-                                  {{$service-> service}}
-                              </a>
-                          </li>
-                      @endforeach
+      <footer>
+        @include('pages.components.footer')
+      </footer>
 
-                  </ul> --}}
-                  <span>
-                      <a href="#" @click="service()">prova</a>
-                  </span>
-                  <ul>
-                      <li v-for="elem in allservice" @click="upservice(elem.id)">
-                          @{{elem.service}}
+    </div> {{-- FINE DI "SEARCH" --}}
 
 
-                      </li>
-                  </ul>
-              </div>
-          </header>
-          <main>
-              <div class="search-page">
-
-                  <!-- UPPER SECTION STARTS HERE -->
-                  <div class="left-section"  style="display: flex; flex-direction:column">
-                    @foreach ($apartments as $item)
-                        <div v-if="currentapartment.length<1" style="display: flex; height:200px; margin:5px; border-bottom: 1px solid lightgray">
-                            <div style="flex-basis: 50%">
-                                <img src="{{$item->cover_image}}" alt="immagine stanza" style="width:100%; border-radius:10px" >
-                            </div>
-                            <div  style="display: flex; flex-direction:column; margin:5px; flex-basis:50%">
-                                        {{-- <a href="{{route('apartment', $item->id)}}"> --}}
-                                        <a @click="redirect(elem.id)">
-                                            <h2>
-                                                {{$item->title}}
-                                            </h2>
-                                        </a>
-                                        <a href="#" onclick="makemarker({{$item -> longitude}},{{$item -> latitude}})">
-                                            {{$item->address}}
-                                        </a>
-                                        <span>Area : <span style:"font-weight:bolder">{{$item->area}}  m^2</span></span>
-                                        <span>Numberi di posti letto: {{$item->number_beds}}</span>
-                                        <span>Numbero di stanze: {{$item->number_rooms}}</span>
-
-                            </div>
-
-
-
-
-                        </div>
-                    @endforeach
-                        <div else  v-for="elem in currentapartment" style="display: flex; height:200px; margin:5px; border-bottom: 1px solid lightgray">
-                            <div style="flex-basis: 50%">
-                                <img :src=" elem.cover_image " alt="immagine stanza" style="width:100%; border-radius:10px" >
-                            </div>
-                            <div  style="display: flex; flex-direction:column; margin:5px; flex-basis:50%">
-                                        <a href="{{route('apartment',1)}}" >
-                                            <h2>
-                                                @{{ elem.title }}
-                                            </h2>
-                                        </a>
-                                        <a href="#" onclick="makemarker({{$item -> longitude}},{{$item -> latitude}})">
-                                            @{{elem.address}}
-                                        </a>
-                                        <span>Area : <span style:"font-weight:bolder">@{{elem.area}}  m^2</span></span>
-                                        <span>Numberi di posti letto: @{{elem.number_beds}}</span>
-                                        <span>Numbero di stanze: @{{elem.number_rooms}}</span>
-                            {{-- <strong else v-for="i in currentapartment">
-                                <a href="{{route('apartment', $item->id)}}">@{{ i.title }}</a><br>
-                            </strong> --}}
-                            {{-- prove mappa lat long
-                                <a href="#" onclick="makemarker(15.06619,37.54305)">Defautl</a>
-                                <a href="#" @click="log()">Prova</a>--}}
-
-                            </div>
-
-                        </div>
-                        {{-- <ul>
-                            <li>latitude e longitude </li>
-                            @foreach ($apartments as $item)
-                                <li>
-                                    <a href="#"
-                                    onclick="makemarker({{$item -> longitude}},{{$item -> latitude}})"
-                                    style="background-color:lightgray;padding:0.5rem;border-radius:1rem;padding-bottom:2px; ">
-                                        {{$item -> latitude}} {{$item -> longitude}}
-                                    </a>
-                                </li>
-                            @endforeach
-                        </ul> --}}
-
-                  </div>
-                  <div class="right-section">
-                      <div id='map' class='map'></div>
-                  </div>
-                  </div>
-
-                  <!-- INIZIO PARTE DI DESTRA CON IMMAGINE GRANDE (CALABRIA)-->
-                  <!-- FINE PARTE DI DESTRA CON IMMAGINE GRANDE (CALABRIA)-->
-
-              </div>
-          </main>
-          <footer>
-            @include('pages.components.footer')
-          </footer>
-      </div>
     <script>
         // esempio di creare una funzione che metta tutti i marker nella mappa
         function makemarker(LNG, LAT){
--- resources/views/pages/search.blade.php
+++ resources/views/pages/search.blade.php
@@ -1,1 +1,89 @@
 @extends('layouts.search_layout')
+@section('content')
+  <main>
+    <div class="search-page">
+
+      <div class="left-section"  style="display: flex; flex-direction:column">
+        @foreach ($apartments as $item)
+
+          <div v-if="currentapartment.length<1" style="display: flex; height:200px; margin:5px; border-bottom: 1px solid lightgray">
+
+            <div style="flex-basis: 50%">
+              <img src="{{$item->cover_image}}" alt="immagine stanza" style="width:100%; border-radius:10px" >
+            </div>
+
+
+            <div  style="display: flex; flex-direction:column; margin:5px; flex-basis:50%">
+              {{-- <a href="{{route('apartment', $item->id)}}"> --}}
+              <a @click="redirect(elem.id)">
+                <h2>{{$item->title}}</h2>
+              </a>
+
+              <a href="#" onclick="makemarker({{$item -> longitude}},{{$item -> latitude}})">
+                {{$item->address}}
+              </a>
+
+              <span>Area : <span style:"font-weight:bolder">{{$item->area}}  m^2</span></span>
+              <span>Numberi di posti letto: {{$item->number_beds}}</span>
+              <span>Numbero di stanze: {{$item->number_rooms}}</span>
+
+            </div>
+
+          </div>
+        @endforeach
+
+        <div else  v-for="elem in currentapartment" style="display: flex; height:200px; margin:5px; border-bottom: 1px solid lightgray">
+
+          <div style="flex-basis: 50%">
+            <img :src=" elem.cover_image " alt="immagine stanza" style="width:100%; border-radius:10px" >
+          </div>
+
+          <div  style="display: flex; flex-direction:column; margin:5px; flex-basis:50%">
+
+            <a href="{{route('apartment',1)}}" >
+              <h2>@{{ elem.title }}</h2>
+            </a>
+
+            <a href="#" onclick="makemarker({{$item -> longitude}},{{$item -> latitude}})">
+              @{{elem.address}}
+            </a>
+
+            <span>Area : <span style:"font-weight:bolder">@{{elem.area}}  m^2</span></span>
+            <span>Numberi di posti letto: @{{elem.number_beds}}</span>
+            <span>Numbero di stanze: @{{elem.number_rooms}}</span>
+
+            {{-- <strong else v-for="i in currentapartment">
+            <a href="{{route('apartment', $item->id)}}">@{{ i.title }}</a><br>
+            </strong> --}}
+            {{-- prove mappa lat long
+            <a href="#" onclick="makemarker(15.06619,37.54305)">Defautl</a>
+            <a href="#" @click="log()">Prova</a>--}}
+
+          </div>
+
+        </div>
+          {{-- <ul>
+          <li>latitude e longitude </li>
+          @foreach ($apartments as $item)
+          <li>
+          <a href="#"
+          onclick="makemarker({{$item -> longitude}},{{$item -> latitude}})"
+          style="background-color:lightgray;padding:0.5rem;border-radius:1rem;padding-bottom:2px; ">
+          {{$item -> latitude}} {{$item -> longitude}}
+          </a>
+          </li>
+          @endforeach
+          </ul> --}}
+      </div>
+
+      <div class="right-section">
+
+        <div id='map' class='map'>
+        </div>
+
+      </div>
+
+    </div>
+  </main>
+
+@endsection
