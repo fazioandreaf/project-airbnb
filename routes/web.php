c21fef0e9daf44152c59f735ae140a2482851a84
titty91 <tznbaraldi@gmail.com>
2021-06-30 16:23:27 +0200

    comm tiz

--- routes/web.php
+++ routes/web.php
@@ -59,4 +59,3 @@ Route::get('/pages/maps', 'MainController@maps');
 // Route::get('/add_sponsor/{id}','MainController@addSponsor')->name('add_sponsor');
 // Route::get('/statistic/{id}', 'MainController@statistic') -> name('statistic');
 
-Route::view('/pay', 'pages.pay');
