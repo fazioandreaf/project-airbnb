3fab59d69da6a21928cadd95f27d6fe764c40f04
Gioele <gioele.armiento@gmail.com>
2021-07-03 17:25:17 +0200

    Validazione edit aggiunta

--- resources/js/app.js
+++ resources/js/app.js
@@ -65,6 +65,96 @@ document.addEventListener('DOMContentLoaded', () => {
 
                 this.dropdownActive = false;
             },
+            
+            validateEditApartment: function(e) {
+                this.formErrors = [];
+                const hasNumbers = /\d/;
+                const numbers = /^[0-9]+$/;
+                const scrollTo = document.getElementById('edit-section');
+                let input = [];
+
+                // Apartment Title Validation
+                if(!this.title) {
+                    this.formErrors.push("Il campo titolo è obbligatorio!");
+                    input.push("title");
+                    
+                } else if(this.title.length < 4) {
+                    this.formErrors.push("Il campo titolo deve avere almeno 4 caratteri!");
+                    input.push("title");
+                } else if(this.title.length > 128) {
+                    this.formErrors.push("Hai superato il limite di 128 caratteri per il campo titolo!");
+                    input.push("title");
+                }
+
+                // Description Validation
+                if(this.description && this.description.length < 22) {
+                    this.formErrors.push("Il campo descrizione deve avere almeno 22 caratteri!");
+                    input.push("description");
+                }
+
+                // Rooms validation
+                if(!this.rooms) {
+                    this.formErrors.push("Il campo numero camere è obbligatorio!")
+                    input.push("number_rooms");
+                } else if(!numbers.test(this.rooms)) {
+                    this.formErrors.push("Il campo numero camere deve essere numerico!");
+                    input.push("number_rooms");
+                } else if(this.rooms < 1) {
+                    this.formErrors.push("Il tuo appartamento deve avere almeno 1 stanza!");
+                    input.push("number_rooms");
+                }
+
+                // Bathrooms validation
+                if(!this.toiletes) {
+                    this.formErrors.push("Il campo numero bagni è obbligatorio!");
+                    input.push("number_toiletes");
+                } else if(!numbers.test(this.toiletes)) {
+                    this.formErrors.push("Il campo numero bagni deve essere numerico!");
+                    input.push("number_toiletes");
+                } else if(this.toiletes < 1) {
+                    this.formErrors.push("Il tuo appartamento deve avere almeno 1 bagno!");
+                    input.push("number_toiletes");
+                }
+
+                // Beds validation
+                if(!this.beds) {
+                    this.formErrors.push("Il campo numero letti è obbligatorio!");
+                    input.push("number_beds");
+                } else if(!numbers.test(this.beds)) {
+                    this.formErrors.push("Il campo numero letti deve essere numerico!");
+                    input.push("number_beds");
+                } else if(this.beds < 1) {
+                    this.formErrors.push("Il tuo appartamento deve avere almeno 1 letto!");
+                    input.push("number_beds");
+                }
+
+                // Area validation
+                if(!this.area) {
+                    this.formErrors.push("Il campo metri quadri è obbligatorio!");
+                    input.push("area");
+                } else if(!numbers.test(this.area)) {
+                    this.formErrors.push("Il campo metri quadri deve essere numerico!");
+                    input.push("area");
+                } else if(this.area < 1) {
+                    this.formErrors.push("Devi avere almeno un metro quadro di spazio!");
+                    input.push("area");
+                }
+
+                // Address validation
+                if(!this.address) {
+                    this.formErrors.push("Il campo indirizzo è obbligatorio!");
+                    input.push("address");
+                }
+
+                if(!this.formErrors.length) {
+                    return true
+                }
+
+                this.classes = input;
+                scrollTo.scrollIntoView({behavior: "smooth"});
+                console.log(this.formErrors);
+                e.preventDefault();
+            },
 
             validateNewApartment: function(e) {
 
