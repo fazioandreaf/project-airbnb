75d49e11dbd2c283bee4604f49ab5d2db7f4d56b
Danny <danny.brogno@gmail.com>
2021-07-03 23:03:14 +0200

    late night changes

--- resources/views/pages/apartment.blade.php
+++ resources/views/pages/apartment.blade.php
@@ -68,200 +68,202 @@
 
     {{-- _________JUMBOTRON INIZIA QUI__________ --}}
 
-      <div class="jumbotron-apartment">
+    <div class="jumbotron-apartment">
 
-        <div id="jumbo-container">
+      <div id="jumbo-container">
 
-          <div class="titolo">
-            <h1>
+        <div class="titolo">
+          <h1>
+            <a href="#">
+              {{$apartment->title}}
+            </a>
+          </h1>
+        </div>
+
+        {{-- INIZIO DESCRIZIONE --}}
+        <div class="description-apartment">
+          <div>
+            <span>
               <a href="#">
-                {{$apartment->title}}
+                {{$apartment->address}}
               </a>
-              {{-- <div>ID:[{{$apartment->id}}]</div> --}}
-            </h1>
-
+            </span>
           </div>
+        </div>
+        {{-- FINE DESCRIZIONE --}}
 
-          {{-- INIZIO DESCRIZIONE --}}
-          <div class="description-apartment">
+        {{-- _________FINE DESCRIZIONE - INIZIO IMMAGINI___________ --}}
 
-            <div>
-              <span>
-                <a href="#">
-                  {{$apartment->address}}
-                </a>
-              </span>
-            </div>
+        {{-- INIZIO immagini appartamento --}}
+        <div class="apartment-img-container">
 
+          {{-- INIZIO IMMAGINE GRANDE --}}
+          <div>
+            <a href="#">
+              <img class="immagine-grande" src="{{asset('/storage/assets/'.$apartment->cover_image)}}" alt="immagine-qui" style="width: 100%;height: 100%;">
+            </a>
           </div>
-          {{-- FINE DESCRIZIONE --}}
+          {{-- FINE IMMAGINE GRANDE --}}
 
-          {{-- _________FINE DESCRIZIONE - INIZIO IMMAGINI___________ --}}
-
-          {{-- INIZIO immagini appartamento --}}
-          <div class="apartment-img-container">
-            {{-- v-for="image in imgArr" --}}
-
-            {{-- INIZIO IMMAGINE GRANDE --}}
+          {{-- INIZIO 4 IMMAGINI --}}
+          <div>
             <div>
               <a href="#">
-                <img class="immagine-grande" src="{{asset('/storage/assets/'.$apartment->cover_image)}}" alt="immagine-qui" style="width: 100%;height: 100%;">
+                <img class="immagini-piccole"
+                  src="https://source.unsplash.com/375x245/?interior,Room,Home"
+                  alt="immagine-qui" style="width: 100%;height: 100%;">
               </a>
             </div>
-            {{-- FINE IMMAGINE GRANDE --}}
 
-            {{-- INIZIO 4 IMMAGINI --}}
             <div>
-              <div>
-                <a href="#">
-                  <img class="immagini-piccole"
-                    src="https://source.unsplash.com/375x245/?interior,Room,Home" 
-                    alt="immagine-qui" style="width: 100%;height: 100%;">
-                </a>
-              </div>
-
-              <div>
-                <a href="#">
-                  <img class="immagini-piccole"
-                    src="https://source.unsplash.com/375x245/?Kitchen,Interior" 
-                    alt="immagine-qui" style="width: 100%;height: 100%;">
-                </a>
-              </div>
-
-              <div>
-                <a href="#">
-                  <img class="immagini-piccole"
-                    src="https://source.unsplash.com/collection/2048325/375x245" 
-                    alt="immagine-qui" style="width: 100%;height: 100%;">
-                </a>
-              </div>
-
-              <div>
-                <a href="#">
-                  <img class="immagini-piccole"
-                    src="https://source.unsplash.com/375x245/?Bathroom" 
-                    alt="immagine-qui" style="width: 100%;height: 100%;">
-                </a>
-              </div>
+              <a href="#">
+                <img class="immagini-piccole"
+                  src="https://source.unsplash.com/375x245/?Kitchen,Interior"
+                  alt="immagine-qui" style="width: 100%;height: 100%;">
+              </a>
             </div>
-            {{-- INIZIO 4 IMMAGINI --}}
 
-            {{-- <img src="{{asset('storage/apartment-img/'.$apartment -> cover_image)}}" alt=""> --}}
+            <div>
+              <a href="#">
+                <img class="immagini-piccole"
+                  src="https://source.unsplash.com/collection/2048325/375x245"
+                  alt="immagine-qui" style="width: 100%;height: 100%;">
+              </a>
+            </div>
+
+            <div>
+              <a href="#">
+                <img class="immagini-piccole"
+                  src="https://source.unsplash.com/375x245/?Bathroom"
+                  alt="immagine-qui" style="width: 100%;height: 100%;">
+              </a>
+            </div>
           </div>
-          {{-- FINE immagini appartamento --}}
+          {{-- FINE 4 IMMAGINI --}}
+        </div>
 
 
-          {{-- INIZIO SEZIONE DETTAGLI FLAT (SOTTO IMMAGINI) --}}
-          <div class="dettagli-flat">
+        {{-- INIZIO SEZIONE DETTAGLI FLAT (SOTTO IMMAGINI) --}}
+        <div class="dettagli-flat">
 
-            {{-- INIZIO SEZIONE DI SINISTRA CON ICONE --}}
-            <div class="sinistra-dettagli-flat">
+          {{-- INIZIO SEZIONE DI SINISTRA CON ICONE --}}
+          <div class="sinistra-dettagli-flat">
 
-              <h2>
-                <a href="#">
-                  TITOLO ANNUNCIO
-                </a>
-              </h2>
+            <h2>
+              <a href="#">
+                TITOLO ANNUNCIO
+              </a>
+            </h2>
 
-              <ul id="all-four-rows">
+            <ul id="all-four-rows">
 
-                {{-- (1) --}}
-                <li class="riga uno">
-                  <p>{{$apartment->description}}.</p>
-                </li>
+              {{-- (1) --}}
+              <li class="riga uno">
+                <p>{{$apartment->description}}.</p>
+              </li>
 
-                {{-- (2) --}}
-                <li class="riga due">
+              {{-- (2) --}}
+              <li class="riga due">
 
-                  <span>
-                    <img src="{{asset('storage/assets/icone_stanze.png')}}" alt="stanze-img">
-                    <a href="#">
-                      Stanze :
-                      {{$apartment->number_rooms}}
-                    </a>
-                  </span>
+                <span>
+                  <img src="{{asset('storage/assets/icone_stanze.png')}}" alt="stanze-img">
+                  <a href="#">
+                    Stanze :
+                    {{$apartment->number_rooms}}
+                  </a>
+                </span>
 
-                  <span>
-                    <img src="{{asset('storage/assets/icone_letto.png')}}" alt="letto-img" style="width: 50px; height: 50px;">
-                    <a href="#">
-                      Letti :
-                      {{$apartment->number_beds}}
-                    </a>
-                  </span>
+                <span>
+                  <img src="{{asset('storage/assets/icone_letto.png')}}" alt="letto-img" style="width: 50px; height: 50px;">
+                  <a href="#">
+                    Letti :
+                    {{$apartment->number_beds}}
+                  </a>
+                </span>
 
-                  <span>
-                    <img src="{{asset('storage/assets/icone_bagno.png')}}" alt="bagno-img" style="width: 50px; height: 50px;">
-                    <a href="#">
-                      Numero bagni :
-                      {{$apartment->number_toiletes}}
-                    </a>
-                  </span>
+                <span>
+                  <img src="{{asset('storage/assets/icone_bagno.png')}}" alt="bagno-img" style="width: 50px; height: 50px;">
+                  <a href="#">
+                    Numero bagni :
+                    {{$apartment->number_toiletes}}
+                  </a>
+                </span>
 
-                  <span>
-                    <img src="{{asset('storage/assets/icone_area.png')}}" alt="area-img" style="width: 50px; height: 50px;">
-                    <a href="#">
-                      Area :
-                      {{$apartment->area}}mq
-                    </a>
-                  </span>
+                <span>
+                  <img src="{{asset('storage/assets/icone_area.png')}}" alt="area-img" style="width: 50px; height: 50px;">
+                  <a href="#">
+                    Area :
+                    {{$apartment->area}}mq
+                  </a>
+                </span>
 
-                </li>
+              </li>
 
-                {{-- (3) --}}
-                <li class="riga tre">
+              {{-- (3) --}}
+              <li class="riga tre">
 
-                  <span>
-                    <a href="#">
-                      <h3>Servizi extra</h3>
-                        <ul class="sub-list">
-                          @foreach ($apartment->services as $service)
-                          <li>
-                            <img src="{{asset('storage/assets/icone_'. $service->service . '.png')}}" alt="servizio-extra" style="width: 40px; height: 40px;">
-                            {{$service->service}}
-                          </li>
-                        @endforeach
-                        </ul>
-                    </a>
-                  </span>
-                </li>
-
-                {{-- (4) --}}
-                <li class="riga quattro">
-                  <h3>Informazioni utili</h3>
-
-                  <div class="test">
-                    <a href="#">
-                      <img src="{{asset('storage/assets/icone_sponsor.png')}}" alt="sponsor-img" style="width: 50px; height: 50px;">
-                      @foreach ($apartment->sponsors as $apSp)
-                        <p>
-                          {{$apSp->sponsor_duration}} ore di sponsor
-                        </p>
+                <span>
+                  <a href="#">
+                    <h3>
+                      Servizi extra
+                    </h3>
+                    <ul class="sub-list">
+                      @foreach ($apartment->services as $service)
+                        <li>
+                          <img src="{{asset('storage/assets/icone_'. $service->service . '.png')}}" alt="servizio-extra" style="width: 40px; height: 40px;">
+                          {{$service->service}}
+                        </li>
                       @endforeach
-                    </a>
-                    <a href="#">
-                      <i class="fas fa-pump-soap"></i>
-                      <p>Questo host si impegna a seguire la procedura avanzata di pulizia in 5 fasi di BoolB&B.</p>
-                    </a>
-                  </div>
-                </li>
+                    </ul>
+                  </a>
+                </span>
+              </li>
 
-              </ul>
+              {{-- (4) --}}
+              <li class="riga quattro">
 
-            </div>
-            {{-- FINE SEZIONE DI SINISTRA CON ICONE --}}
+                <h3>
+                  Informazioni utili
+                </h3>
+
+                <div class="test">
+
+                  <a href="#">
+                    <img src="{{asset('storage/assets/icone_sponsor.png')}}" alt="sponsor-img" style="width: 50px; height: 50px;">
+                    @foreach ($apartment->sponsors as $apSp)
+                      <p>
+                        {{$apSp->sponsor_duration}} ore di sponsor
+                      </p>
+                    @endforeach
+                  </a>
+
+                  <a href="#">
+                    <i class="fas fa-pump-soap"></i>
+                    <p>
+                      Questo host si impegna a seguire la procedura avanzata di pulizia in 5 fasi di BoolB&B.
+                    </p>
+                  </a>
+
+                </div>
+              </li>
+
+            </ul> {{-- FINE OF ALL-FOUR-ROWS --}}
+
+          </div> {{-- FINE SINISTRA DETTAGLI FLATS --}}
+          {{-- FINE SEZIONE DI SINISTRA CON ICONE --}}
 
 
-            {{-- ___INIZIO SEZIONE DI DESTRA___ --}}
-            <div class="destra-dettagli-flat">
-              <form action="" method="get">
-                <span>Scrivi all'host<i class="fas fa-pencil-alt"></i><div>Vedi se ci sono camere disponibili.</div></span>
-                <textarea rows="20" cols="30"></textarea>
-                <input type="submit" value="Invia">
-              </form>
-            </div>
-            {{-- FINE SEZIONE DI DESTRA --}}
+          {{-- ___INIZIO SEZIONE DI DESTRA___ --}}
+          <div class="destra-dettagli-flat">
+            <form action="" method="get">
+              <span>Scrivi all'host<i class="fas fa-pencil-alt"></i><div>Vedi se ci sono camere disponibili.</div></span>
+              <textarea rows="20" cols="30"></textarea>
+              <input type="submit" value="Invia">
+            </form>
+          </div>
+          {{-- FINE SEZIONE DI DESTRA --}}
 
-          </div> {{-- FINE DETTAGLI-FLAT (width: 100% height: 600px) --}}
+        </div> {{-- FINE DETTAGLI-FLAT (width: 100% height: 600px) --}}
 
       </div> {{-- ______________ FINE JUMBO CONTAINER __________________ --}}
 
