f499647a75dcb8edd6c23322d8fee8c7f76766eb
pasqu <pasquale.bucciero97@gmailcom>
2021-07-06 01:23:39 +0200

    message.blade

--- resources/views/pages/message.blade.php
+++ resources/views/pages/message.blade.php
@@ -1,4 +1,33 @@
-@extends('layouts.main_layout')
+@extends('layouts.minimal-layout')
 @section('content')
-messaggio inviato
+<div id="success-message">
+    <div class="cnt-success-msg">
+        <h2>
+            Transaction successful.
+        </h2>
+        <span>
+            <strong>
+              {{--The ID is: {{$idTransaction}}--}}
+            </strong>
+        </span>
+        <div class="icon-confirm">
+            <div><i class="fas fa-check-circle"></i></div>
+            <div class="box"></div>
+        </div>
+        <span class="text-confirm">
+            Grazie per aver scelto 
+            <strong > 
+                BoolB&B 
+            </strong> 
+        </span>
+        <a href="{{route('myapartment',Auth::id())}}">
+            <button>
+                <b>
+                    Torna agli appartamenti 
+                </b>
+            </button>
+        </a>
+    </div>
+    
+</div>
 @endsection
