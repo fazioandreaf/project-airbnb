e8c704f3410138984f1245bbc483cfee357fef63
Gioele <gioele.armiento@gmail.com>
2021-07-05 21:26:06 +0200

    Testo nella search cambiato in Nunito <3

--- public/js/search.js
+++ public/js/search.js
@@ -2518,7 +2518,7 @@ document.addEventListener("DOMContentLoaded", function () {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Danny\Progetti_2021\07-2021\06-07-2021\project-airbnb\resources\js\search.js */"./resources/js/search.js");
+module.exports = __webpack_require__(/*! C:\Boolean\project-airbnb\resources\js\search.js */"./resources/js/search.js");
 
 
 /***/ })
